28ef62cbd2502dc4f7e0596f3ff0484b97705cf4
Maciek Sakrejda <maciek@heroku.com>
2014-07-01 13:56:34 -0700

    Add POST transfers endpoint

--- lib/endpoints/transfers.rb
+++ lib/endpoints/transfers.rb
@@ -2,6 +2,30 @@ module Transferatu::Endpoints
   class Transfers < Base
     include Authenticator
 
+    def serializer
+      @serializer ||= Transferatu::Serializers::Transfer.new(:default)
+    end
+
+    def serialize(transfer)
+      serializer.serialize(transfer)
+    end
+
+    def self.schema
+      @@schema ||= File.read("docs/schema.json")
+    end
+
+    use Committee::Middleware::RequestValidation,
+        schema: schema, strict: true
+
+    helpers do
+      def data
+        env["committee.params"]
+      end
+      def group
+        Transferatu::Group.find(user: current_user, name: data["group"]["name"])
+      end
+    end
+
     namespace "/transfers" do
       before do
         content_type :json
@@ -13,8 +37,15 @@ module Transferatu::Endpoints
       end
 
       post do
+        transfer = Transferatu::Mediators::Transfers::Creator.run(
+                   group: group,
+                   type: data["type"],
+                   from_url: data["from_url"],
+                   to_url: data["to_url"],
+                   options: data["options"] || {}
+                 )
         status 201
-        "{}"
+        respond serialize(transfer)
       end
 
       get "/:id" do

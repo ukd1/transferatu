472249609ad1c60d8330753955d6d4d7ad8f83c1
Maciek Sakrejda <maciek@heroku.com>
2014-07-02 11:20:56 -0700

    Flesh out transfers endpoint tests

--- spec/endpoints/transfers_spec.rb
+++ spec/endpoints/transfers_spec.rb
@@ -11,14 +11,37 @@ module Transferatu::Endpoints
     before do
       password = 'passw0rd'
       @user = create(:user, password: password)
-      authorize @user.name, password
       @group = create(:group, user: @user)
+      authorize @user.name, password
     end
 
     describe "GET /groups/:name/transfers" do
-      it "succeeds" do
+      it "lists transfers for the group" do
         get "/groups/#{@group.name}/transfers"
         last_response.status.should eq(200)
+      end
+    end
+
+    describe "GET /groups/:name/transfers/:id" do
+      let(:xfer) { create(:transfer, group: @group) }
+
+      it "succeeds" do
+        get "/groups/#{@group.name}/transfers/#{xfer.uuid}"
+        last_response.status.should eq(200)
+      end
+    end
+
+    describe "POST /groups/:name/transfers" do
+      before do
+        header "Content-Type", "application/json"
+      end
+      it "succeeds" do
+        post "/groups/#{@group.name}/transfers", JSON.generate(
+                                                 type:     'pg_dump:pg_restore',
+                                                 from_url: 'postgres:///test1',
+                                                 to_url:   'postgres:///test2'
+                                               )
+        last_response.status.should eq(201)
       end
     end
   end

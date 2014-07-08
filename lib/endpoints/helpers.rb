83463ff2f9bda5fe762c6cc563f5e7f3e5b39f25
Maciek Sakrejda <maciek@heroku.com>
2014-07-08 15:08:29 -0700

    Remove obsolete comment

--- lib/endpoints/helpers.rb
+++ lib/endpoints/helpers.rb
@@ -4,7 +4,6 @@ module Transferatu
       attr_reader :current_user
 
       def authenticate
-        # TODO: this authentication check is pretty crufty
         auth = Rack::Auth::Basic::Request.new(request.env)
         unless auth.provided? && auth.basic? && auth.credentials
           throw(:halt, [401, "Not Authorized\n"])

0c0674a30c44de6c992175d0a00896bd53566f26
Maciek Sakrejda <maciek@heroku.com>
2014-07-23 00:02:29 -0700

    Fix schedule serialization

--- lib/serializers/schedule_serializer.rb
+++ lib/serializers/schedule_serializer.rb
@@ -1,7 +1,17 @@
 module Transferatu::Serializers
   class Schedule < Base
-    structure(:default) do |arg|
+    structure(:default) do |schedule|
       {
+        uuid: schedule.uuid,
+        name: schedule.name,
+
+        hour:     schedule.hour,
+        days:     schedule.dows.map { |d| Date::DAYNAMES[d] },
+        timezone: schedule.timezone,
+
+        created_at: schedule.created_at,
+        updated_at: schedule.updated_at,
+        deleted_at: schedule.deleted_at
       }
     end
   end

b1f59b3be81bb8819123bb3ad62a3ba382c1498e
Maciek Sakrejda <maciek@heroku.com>
2014-08-20 12:50:25 -0700

    Add transfer state metrics job

--- clock.rb
+++ clock.rb
@@ -6,9 +6,24 @@ require "clockwork"
 
 $stdout.sync = true
 
+class LogStub
+  # Pliny does not make it easy to log without actually including the
+  # module right now, so we have this as a workaround
+  include Pliny::Log
+end
+
 module Clockwork
+  logger = LogStub.new
+
   every(1.minute, "top-off-workers") do
     Transferatu::WorkerManager.new.top_off_workers
+  end
+
+  every(1.minute, "log-metrics") do
+    pending_xfer_count = Transferatu::Transfer.pending.count
+    active_xfer_count = Transferatu::Transfer.in_progress.count
+    logger.log(:"sample#pending_xfer_count" => pending_xfer_count,
+               :"sample#active_xfer_count" => active_xfer_count)
   end
 
   every(4.hours, "mark-restart") do

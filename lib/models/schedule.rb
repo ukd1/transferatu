6f38c74420706bfc29dab895fc91c40ae90940e3
Maciek Sakrejda <maciek@heroku.com>
2014-08-20 18:17:46 -0700

    Add pending_for Schedule method

--- lib/models/schedule.rb
+++ lib/models/schedule.rb
@@ -5,5 +5,21 @@ module Transferatu
 
     many_to_one :group
     one_to_many :transfers
+
+    def_dataset_method(:pending_for) do |time|
+      self.with_sql(<<-EOF, time: time)
+SELECT
+  s.*
+FROM
+  schedules s INNER JOIN groups g ON s.group_id = g.uuid
+    LEFT OUTER JOIN transfers t ON t.schedule_id = s.uuid
+      AND t.created_at > (timestamptz :time - interval '12 hours')
+WHERE
+  ARRAY[extract(dow from (:time at time zone timezone)::timestamptz)::smallint] && dows
+    AND hour = extract(hour from (:time at time zone timezone)::timestamptz)
+    AND t.uuid IS NULL;
+EOF
+    end
+
   end
 end
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,6 +1,55 @@
 require "spec_helper"
 
 describe Transferatu::Schedule do
-  
+  describe ".pending_for" do
+    let(:scheduled_time) { Time.new(2014, 8, 20, 15, 0, 0, 0) } # this is a Wednesday, dow 3
+
+    it "includes schedules for this day, time, and timezone" do
+      s = create(:schedule, hour: 15, dows: [ 3 ], timezone: 'UTC')
+      scheds = Transferatu::Schedule.pending_for(scheduled_time).all
+      expect(scheds.count).to eq(1)
+      expect(scheds.first.uuid).to eq(s.uuid)
+    end
+
+    it "includes schedules for this and other days, this time and timezone" do
+      s = create(:schedule, hour: 15, dows: [ 1, 3, 5 ], timezone: 'UTC')
+      scheds = Transferatu::Schedule.pending_for(scheduled_time).all
+      expect(scheds.count).to eq(1)
+      expect(scheds.first.uuid).to eq(s.uuid)
+    end
+
+    it "omits schedules for this day but another time in this timezone" do
+      s = create(:schedule, hour: 14, dows: [ 3 ], timezone: 'UTC')
+      scheds = Transferatu::Schedule.pending_for(scheduled_time).all
+      expect(scheds).to be_empty
+    end
+
+    it "omits schedules for this day and time in another timezone" do
+      s = create(:schedule, hour: 15, dows: [ 3 ], timezone: 'America/Los_Angeles')
+      scheds = Transferatu::Schedule.pending_for(scheduled_time).all
+      expect(scheds).to be_empty
+    end
+
+    it "omits schedules for a different day at this time in this timezone" do
+      s = create(:schedule, hour: 15, dows: [ 4 ], timezone: 'UTC')
+      scheds = Transferatu::Schedule.pending_for(scheduled_time).all
+      expect(scheds).to be_empty
+    end
+
+    it "includes schedules for this day and an equivalent time in a different timezone" do
+      s = create(:schedule, hour: 18, dows: [ 3 ], timezone: 'Africa/Addis_Ababa')
+      scheds = Transferatu::Schedule.pending_for(scheduled_time).all
+      expect(scheds.count).to eq(1)
+      expect(scheds.first.uuid).to eq(s.uuid)
+    end
+
+    it "includes schedules for an equivalent day and time in a different timezone" do
+      s = create(:schedule, hour: 1, dows: [ 4 ], timezone: 'Asia/Khandyga')
+      scheds = Transferatu::Schedule.pending_for(scheduled_time).all
+      expect(scheds.count).to eq(1)
+      expect(scheds.first.uuid).to eq(s.uuid)
+    end
+
+  end
 end
 

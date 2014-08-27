b9361d580b0ca574090cfd91467fcf22a4fdbd48
Maciek Sakrejda <maciek@heroku.com>
2014-08-27 13:12:12 -0700

    Don't try to log to logplex when log_input_url is nil

--- lib/models/group.rb
+++ lib/models/group.rb
@@ -13,7 +13,9 @@ module Transferatu
       # N.B.: we leak two threads per logplex endpoint here due to
       # the structure of lpxc, but given the low number of endpoints,
       # this is tolerable
-      Lpxc.puts(message, log_input_url, procid: Config.logplex_procid)
+      unless log_input_url.nil?
+        Lpxc.puts(message, log_input_url, procid: Config.logplex_procid)
+      end
     end
   end
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -4,11 +4,24 @@ describe Transferatu::Group do
   describe "#log" do
     let(:group) { create(:group) }
 
-    it "should use Lpxc to log to logplex" do
-      message = "hello world"
-      expect(Lpxc).to receive(:puts)
-        .with(message, group.log_input_url, procid: Config.logplex_procid)
-      group.log message
+    context "with non-nil log_input_url" do
+      it "should use Lpxc to log to logplex" do
+        message = "hello world"
+        expect(Lpxc).to receive(:puts)
+          .with(message, group.log_input_url, procid: Config.logplex_procid)
+        group.log message
+      end
+    end
+
+    context "with nil log_input_url" do
+      before do
+        group.update(log_input_url: nil)
+      end
+      it "should do nothing" do
+        message = "hello world"
+        expect(Lpxc).to_not receive(:puts)
+        group.log message
+      end
     end
   end
 end

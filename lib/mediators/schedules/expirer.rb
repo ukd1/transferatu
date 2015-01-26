3e78a7af09823f4b1c872212816d26995e9b7b41
Maciek Sakrejda <maciek@heroku.com>
2015-01-25 23:04:12 -0800

    Make scheduled backup retention more generic.

--- lib/mediators/schedules/expirer.rb
+++ lib/mediators/schedules/expirer.rb
@@ -12,23 +12,31 @@ module Transferatu
         # backup available for that time period.
         #
         # So, our strategy is to delete everything that's older than a
-        # week, *except* the oldest backup newer than 2, 3, 4, 5
-        # weeks. We accept a reference time instead of using now() for
-        # repeatability and testing.
+        # week, *except* the oldest backup newer than each of n weeks
+        # (as specified by the schedule) and m months (also as
+        # specified by the schedule). We accept a reference time
+        # instead of using now() for repeatability and testing.
         expired = Transfer.with_sql(<<-EOF, schedule_id: @schedule.uuid, expire_at: @expire_at).all
 SELECT
   t.*
 FROM
-  transfers t
+  transfers t INNER JOIN schedules s ON t.schedule_id = s.uuid
 WHERE
-  t.schedule_id = :schedule_id AND deleted_at IS NULL
+  s.uuid = :schedule_id AND t.deleted_at IS NULL
     AND t.created_at < (timestamptz :expire_at - interval '1 week')
     AND t.uuid NOT IN
 (WITH thresholds AS (
   SELECT
     ts as threshold
   FROM
-    generate_series(timestamptz :expire_at - interval '5 weeks', :expire_at, interval '1 week') ts
+    generate_series(timestamptz :expire_at - (s.retain_weeks * interval '1 week'),
+      :expire_at, interval '1 week') ts
+      UNION ALL
+  SELECT
+    ts as threshold
+  FROM
+    generate_series(timestamptz :expire_at - (s.retain_months * interval '1 month'),
+      timestamptz :expire_at - (s.retain_weeks * interval '1 week'), interval '1 month') ts
 )
 SELECT
   DISTINCT ON (threshold) t.uuid

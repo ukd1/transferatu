db405106ad241f2e1949e8d68de485a952ce4155
Maciek Sakrejda <maciek@heroku.com>
2015-04-07 14:18:54 -0700

    Add schedule information to transfer serialization

--- lib/serializers/transfer_serializer.rb
+++ lib/serializers/transfer_serializer.rb
@@ -45,6 +45,8 @@ module Transferatu::Serializers
         deleted_at:  transfer.deleted_at,
         purged_at:   transfer.purged_at,
 
+        schedule: { uuid: transfer.schedule_id },
+
         num_keep: transfer.num_keep
       }
     end
